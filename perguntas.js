623758032a4108ddfcaecf08d383de91aa3de313
leoBorges044 <leonardoborgespaula43124@gmail.com>
2024-11-19 13:46:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'FUTEBOL',
+    'quem foi o campeão da Copa do Mundo de 2006 ?',
+    'a Seleção campeã da Copa de 2006 foi a Italia '
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Qual time foi campeão da libertadores de 2012 ?',
+    'o campeão da libertadores de 2012 foi o CORINTHIANS, que logo em seguida foi campeão Mundial de Clubes!'
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Quem é o maior campeão da libertadores ?',
+    'O maior campeão da libertadores é o Independiente da Argentina com 7 Titulos '
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Qual pais tem mais titulos de libertadores ?',
+    'A Argentina tem 25 titulos ! o Brasil vem logo em seguida com 23 titulos'
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Qual Seleção venceu a Copa do Mundo de 1998 ?',
+    'A equipe vencedora da Copa do Mundo de 1998 foi a Seleção da França!'
+)
